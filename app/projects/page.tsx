b8b9b2174868b0f5c20e11a2fad84b438962068b
Ola Krassowska <ola.krassowska@hotmail.com>
2024-08-23 11:30:41 +0200

    Added comopnent Categories and filled it with github api repos

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,12 +1,36 @@
 'use client'
-
 import Head from "next/head";
 import Nav from "@/components/Nav";
 import FilterPanel from "@/components/FilterPanel";
 import Footer from "@/components/Footer";
 import Projects from "@/components/Projects";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+
+    const [projects, setProjects] = useState([]);
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        async function fetchProjects() {
+            try {
+                const res = await fetch('https://api.github.com/users/ola191/repos');
+                if (!res.ok) {
+                    throw new Error("Failed to fetch data");
+                }
+                const data = await res.json();
+                console.log(data);
+                setProjects(data);
+                setLoading(false);
+            } catch (error) {
+                console.error("Error fetching data:", error);
+                setLoading(false);
+            }
+        }
+        fetchProjects();
+    }, [])
+    
+
     return (
         <>
             <Head>
@@ -18,7 +42,10 @@ export default function Home() {
             <div className="flex w-4/5 mx-auto h-auto relative flex-col">
                 <Nav />
                 <FilterPanel />
-                <Projects />
+                {loading ? <div>Loading...</div> : 
+                (
+                    <Projects projects={projects} />
+                )}
                 <Footer />
             </div>
             </>
--- components/Projects.jsx
+++ components/Projects.jsx
@@ -1,13 +1,50 @@
-import Image from "next/image";
-import Link from "next/link";
+import React from "react";
 
-export default function Projects() {
+function formatDate(dateString) {
+    const date = new Date(dateString);
 
+    const optionsDate = { year: 'numeric', month: 'long', day: 'numeric' };
+    const optionsTime = { hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false };
+
+    const formattedDate = date.toLocaleDateString('en-GB', optionsDate);
+    const formattedTime = date.toLocaleTimeString('en-GB', optionsTime);
+
+    return `${formattedDate}, ${formattedTime}`;
+}
+
+
+export default function Projects({ projects }) {
     return (
-        <>
-        <section className="flex w-full h-96 border-borderColor border-4 rounded-md flex-row items-center justify-between z-10">
-        
+        <section className="flex w-full flex-wrap gap-y-8 p-8 border-borderColor border-4 rounded-md flex-row justify-between z-10">
+            {projects.map((project, index) => {
+                const descriptionIsEmpty = project.description === null;
+                const description = project.description || "";
+                
+                const [metadata, ...descriptionParts] = description.split(" - ");
+                const mainDescription = descriptionParts.join(" - ");
+                
+                return (
+                    <div key={index} className={`paper w-[350px] h-[450px] bg-borderColor rounded-md flex flex-col p-4 ${descriptionIsEmpty ? "hidden" : ""}`}>
+                        <p className="font-semibold text-lg text-gray-800 p-2">{project.name}</p>
+                        <p className="text-gray-600 p-2 -translate-x-2">{metadata}</p>
+                        <p className="text-gray-600 p-2">{mainDescription}</p>
+                        <p className="text-gray-600 p-2">Created: {formatDate(project.created_at)}</p>
+                        <p className="text-gray-600 p-2">Updated: {formatDate(project.updated_at)}</p>
+                        <p className="text-gray-600 p-2">{project.language}</p>
+                        <p key={index} className="text-gray-600 p-2">
+                        {project.topics && project.topics.length > 0 ? (
+                                project.topics.map((topic, topicIndex) => (
+                                    <span key={topicIndex} className="text-gray-600 p-2">
+                                        {topic}
+                                    </span>
+                                ))
+                            ) : (
+                                <span className="text-gray-600 p-2">No topics available</span>
+                            )}
+                        </p>
+                    </div>
+                );
+            })}
         </section>
-        </>
     );
 }
